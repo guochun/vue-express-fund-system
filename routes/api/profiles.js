e4e0f3fcd06f3248d51055af0dc5ee951c399e28
purcy <purcyguo@gmail.com>
2019-01-02 23:58:50 +0800

    配置删除信息和编辑信息接口

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -47,11 +47,10 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     })
 })
 
-// routes /api/profile/
+// routes /api/profile/:id
 // desc 获取所有的信息
 // access private
 router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-  console.log(req.params.id)
   Profile.findOne({ _id: req.params.id })
     .then((profile) => {
       if (!profile) return Promise.reject({ code: '3001' })
@@ -65,6 +64,43 @@ router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
     })
 })
 
+// routes /api/profile/edit/:id
+// desc 编辑信息
+// access private
+router.post('/edit/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const profileFiled = {}
+  if (req.body.type) profileFiled.type = req.body.type
+  if (req.body.describe) profileFiled.describe = req.body.describe
+  if (req.body.income) profileFiled.income = req.body.income
+  if (req.body.expend) profileFiled.expend = req.body.expend
+  if (req.body.cash) profileFiled.cash = req.body.cash
+  if (req.body.remark) profileFiled.remark = req.body.remark
+  Profile.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: profileFiled},
+    { new: true }
+  ).then((profile) => {
+    res.json(profile)
+  }).catch((err) => {
+    console.log(err)
+  })
 
+})
+
+// routes /api/profile/delete/:id
+// desc 删除当前信息
+// access private
+router.delete('/delete/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findByIdAndRemove(req.params.id)
+    .then((profile) => {
+      return profile.save()
+    })
+    .then((profile) => {
+      res.json(profile)
+    })
+    .catch((err) => {
+      comsole.log(err)
+    })
+})
 
 module.exports = router
